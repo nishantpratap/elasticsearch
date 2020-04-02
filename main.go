371b3252157cefe1bdaf94c9d6e096c294dc318a
nishantpratap <t-nishant.singh@ppbtp.com>
2020-04-02 17:53:14 +0000

    you are doing great

--- main.go
+++ main.go
@@ -14,18 +14,18 @@ import (
         "github.com/elastic/go-elasticsearch/v7"
   esapi "github.com/elastic/go-elasticsearch/v7/esapi"
 )
-type Required struct {
-	//Health       string `json:"health"`
-	//Status       string `json:"status"`
-	Index        string `json:"index"`
-	//UUID         string `json:"uuid"`
-	//Pri          string `json:"pri"`
-	//Rep          string `json:"rep"`
-	//DocsCount    string `json:"docs.count"`
-	//DocsDeleted  string `json:"docs.deleted"`
-	StoreSize    string `json:"store.size"`
-	//PriStoreSize string `json:"pri.store.size"`
-}
+											/*type Required struct {
+												//Health       string `json:"health"`
+												//Status       string `json:"status"`
+												Index        string `json:"index"`
+												//UUID         string `json:"uuid"`
+												//Pri          string `json:"pri"`
+												//Rep          string `json:"rep"`
+												//DocsCount    string `json:"docs.count"`
+												//DocsDeleted  string `json:"docs.deleted"`
+												StoreSize    string `json:"store.size"`
+												//PriStoreSize string `json:"pri.store.size"`
+											}*/
 func main() {
 //initializing my es client
 
@@ -36,71 +36,82 @@ cfg := elasticsearch.Config{
   // ...
 }
 es, _ := elasticsearch.NewClient(cfg)
-//getting cluster info and printing it
-log.Println(es.Info())
-//
+													//getting cluster info and printing it
+													//log.Println(es.Info())
+													//
 req := esapi.CatIndicesRequest{
-//Index:        []string{"stark","lannister"},
-Pretty:       true,
-Human:        true,
+												//Index:        []string{"stark","lannister"},
+												//Pretty:       true,
+												//Human:        true,
 Format:       "json",
 }
 //performing request with client
 res, err := req.Do(context.Background(),es)
 if err != nil {
 log.Fatalf("Error getting response : %s", err)
-}
+} 
 //fmt.Println(res)
 defer res.Body.Close()
 body, err := ioutil.ReadAll(res.Body)
 if err != nil {
     fmt.Println("oops!you have something missing")
 }
-var jsonData [] Required
-err = json.Unmarshal([]byte(body), &jsonData) 
-if err != nil {
-                 panic(err)
-         }
-for _, value := range jsonData {
-		fmt.Println(value)
-	}
-fmt.Println(len(jsonData))
-out, err := json.Marshal(jsonData)
-    if err != nil {
-        panic (err)
-    }
-//fmt.Println(string(out))
-a := string(out)
-res1 := strings.Split(a,",")
-for _, value1 := range res1 {
-		fmt.Println(value1)
-	}
-								//output := make([]string, len(jsonData))
-								//a := string(out)
-								//strArray := strings.Fields(a)
-								//fmt.Println(strArray)
-								//fmt.Printf("%T",strArray)
-								/*for index, each := range strArray {
-										 fmt.Printf("strArray value [%d] is [%s]\n", index, each)
-									 }
-								*/
-								/*re := regexp.MustCompile(`\d{4}-\d{2}-\d{2}`)
-								submatchall := re.FindAllString(a,-1)
-									for _, element := range submatchall {
-										fmt.Println(element)
-									}*/
-								// Declared an empty interface of type Array
+												/*var jsonData [] Required
+												err = json.Unmarshal([]byte(body), &jsonData) 
+												if err != nil {
+														 panic(err)
+													 } */
+												/*for _, value := range jsonData {
+														fmt.Println(value)
+													}*/
+												//fmt.Println(len(jsonData))
+
+													/*out, err := json.Marshal(jsonData)
+													    if err != nil {
+														panic (err)
+													    }
+													//fmt.Println(string(out))
+													a := string(out)
+													res1 := strings.Split(a,",")
+													for _, value1 := range res1 {
+															fmt.Println(value1)
+														}*/
+							
+
+var s [] string	
+// Declared an empty interface of type Array
 var result []map[string]interface{}
 // Unmarshal or Decode the JSON to the interface.
 json.Unmarshal([]byte(body), &result)
 
 for key, result := range result {
-
+                fmt.Printf("%T",result["index"])
 		fmt.Println("Reading Value for INDEX :", key)
 		//Reading each value by its key
 		fmt.Println("IndexName :", result["index"],
 			"|| Health :", result["health"],"|| Size :",result["store.size"])
+                 s = append(s,result["index"].(string),result["store.size"].(string))
        }
+
+/*for e := 0; e < len(s) ; e++ { 
+        fmt.Println(s[e])
+    }*/
+date := "2020.03.29"
+var dfilter [] string
+
+for i := 0; i < len(s) ; i++ {
+if strings.Contains(s[i], date) {
+   //rq := strings.Trim(s[i],"2020.03.29")
+   ram := "Indexname:"+s[i]+"||size="+s[i+1]
+   dfilter = append(dfilter,ram)
+   }
+}
+
+fmt.Println("================================================================================================================")
+for n :=0; n < len(dfilter) ; n++ {
+   fmt.Println(dfilter[n])
+  }
+
 }
 
 
